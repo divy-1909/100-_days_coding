8c6baff5f9958ce7a46b2c7f175d864927867420
divy-1909 <124753015+divy-1909@users.noreply.github.com>
2023-11-10 22:45:35 +0530

    Time: 90 ms (82.89%), Space: 57.5 MB (94.47%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
   bool containsDuplicate(vector<int>& nums) {
-    set<int> s;
-    for (int i = 0; i < nums.size(); i++) {
-      if (s.find(nums[i]) != s.end()) {
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size() - 1; i++) {
+      if (nums[i] == nums[i + 1]) {
         return true;
-      }
-      s.insert(nums[i]);
+      } 
     }
     return false;
   }
